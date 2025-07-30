4993a5217a21f264d762639ea994e60b1433ed38
Natania P <pnatania0@gmail.com>
2025-07-30 17:22:44 -0400

    merged changes

--- calculator.py
+++ calculator.py
@@ -1,34 +1,5 @@
 import math
 
-<<<<<<< HEAD
-def add(a, b):
-    return a + b
-
-<<<<<<< HEAD
-# square root function
-def square_root (a):
-    try:
-        return math.sqrt(a)
-
-    except:
-        if a < 0:
-            raise ValueError
-
-# hypotenuse function
-def hypotenuse(a, b):
-    return math.hypot(a, b)
-
-
-# add function
-def add(a, b): 
-    a + b
-
-
-# sub function
-=======
->>>>>>> a3f1a1d933f4fde3ea7536c81e75bf90d931dd2b
-
-
 def add(a, b):
     return a + b
 
