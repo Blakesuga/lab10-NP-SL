57cf54bd74527edeebb99cd4a9f3059e019f29b5
Seth Lea <hamster@leatribe.com>
2025-07-30 17:30:37 -0400

    updated test calculator

--- test_calculator.py
+++ test_calculator.py
@@ -5,10 +5,18 @@ class TestCalculator(unittest.TestCase):
     ######### Partner 2
     # def test_add(self): # 3 assertions
     #     fill in code
+    def test_add(self):
+        self.assertIs(add(3, 9), 12)
+        self.assertIs(add(5,1),6)
+        self.assertIs(add(4,3), 7)
 
     # def test_subtract(self): # 3 assertions
     #     fill in code
     # ##########################
+    def test_subtract(self):
+        self.assertIs(sub(10, 9), 1)
+        self.assertIs(sub(8, 2), 6)
+        self.assertIs(sub(6, 3),3)
 
     ######## Partner 1
     # def test_multiply(self): # 3 assertions
@@ -24,14 +32,28 @@ class TestCalculator(unittest.TestCase):
     #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
     #     #     div(0, 5)
     #     fill in code
+    def test_divide_by_zero(self):
+        with self.assertRaises(ZeroDivisionError):
+            div(10, 0)
+
 
     # def test_logarithm(self): # 3 assertions
     #     fill in code
+    def test_logarithm(self):
+        self.assertIs(log(100, 10), 2)
+        self.assertIs(log(8, 2), 3)
+        self.assertIs(log(1, 10), 0)
 
     # def test_log_invalid_base(self): # 1 assertion
     #     # use same technique from test_divide_by_zero
     #     fill in code
     # ##########################
+    def test_log_invalid_base(self):
+        with self.assertRaises(ValueError):
+            log(10, 1)
+
+
+
     
     ######## Partner 1
     # def test_log_invalid_argument(self): # 1 assertion
