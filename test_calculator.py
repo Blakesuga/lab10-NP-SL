274f446a2f6b8494192d5e2add7606da05ac9592
Natania P <pnatania0@gmail.com>
2025-07-31 12:00:30 -0400

    updated test_calculator 5

--- test_calculator.py
+++ test_calculator.py
@@ -1,10 +1,10 @@
-#https://github.com/Blakesuga/lab10-NP-SL
+# https://github.com/Blakesuga/lab10-NP-SL
 # Partner 1: Natania Philippe
 # Partner 2: Seth Lea
 
+import math
 import unittest
 from calculator import *
-
 
 
 class TestCalculator(unittest.TestCase):
@@ -16,57 +16,56 @@ class TestCalculator(unittest.TestCase):
         self.assertEqual(add(4,3), 7)
 
     # def test_subtract(self): # 3 assertions
-    #     fill in code
-    # ##########################
     def test_subtract(self):
         self.assertEqual(sub(10, 9), 1)
         self.assertEqual(sub(8, 2), 6)
         self.assertEqual(sub(6, 3),3)
 
+
+
     ######## Partner 1
     def test_multiply(self): # 3 assertions
         self.assertEqual(mul(3, 9), 27)
         self.assertEqual(mul(6, 6), 36)
-        self.assertEqual(mul(4, 24), 108)
+        self.assertEqual(mul(4, 24), 96)
 
     def test_divide(self): # 3 assertions
         self.assertAlmostEqual(div(3, 9), 3)
         self.assertAlmostEqual(div(6, 7), 1.16666667)
-        self.assertRaises(div(0, 7), ZeroDivisionError)
+        with self.assertRaises(ZeroDivisionError):
+            div(0, 8)
 
-    # ##########################
+
 
     ######## Partner 2
     # def test_divide_by_zero(self): # 1 assertion
     def test_divide_by_zero(self):
         with self.assertRaises(ZeroDivisionError):
-            div(10, 0)
-
+            div(0, 10)
 
     # def test_logarithm(self): # 3 assertions
     #     fill in code
     def test_logarithm(self):
-        self.assertEqual(log(100, 10), 2)
-        self.assertEqual(log(8, 2), 3)
-        self.assertEqual(log(1, 10), 0)
+        self.assertEqual(log(10, 100), 2)
+        self.assertEqual(log(2, 8), 3)
+        self.assertEqual(log(10, 1), 0)
 
     # def test_log_invalid_base(self): # 1 assertion
     def test_log_invalid_base(self):
         with self.assertRaises(ValueError):
-            log(10, 1)
+            log(7, -2)
 
 
 
-    
     ######## Partner 1
     def test_log_invalid_argument(self): # 1 assertion
         with self.assertRaises(ValueError):
-            log(0, 5)
+            log(5, -7)
 
     def test_hypotenuse(self): # 3 assertions
-        self.assertEqual(hypotenuse(4, 5), 41)
-        self.assertEqual(hypotenuse(2, 3), 13)
-        self.assertEqual(hypotenuse(7, 4), 65)
+        self.assertEqual(hypotenuse(4, 5), math.sqrt(41))
+        self.assertEqual(hypotenuse(2, 3), math.sqrt(13))
+        self.assertEqual(hypotenuse(7, 4), math.sqrt(65))
 
     def test_sqrt(self): # 3 assertions
         with self.assertRaises(ValueError):
